e51af435886dca10b31e814d390c05c8ed57262b
JorgeLopez3092 <34284767+JorgeLopez3092@users.noreply.github.com>
2019-11-09 23:03:13 -0800

    home set up
    Set up at home

--- routes/project.js
+++ routes/project.js
@@ -9,19 +9,14 @@ router.get('/', ( req, res ) => {
 
 router.get('/:id', (req, res, next) => {
     let { id } = req.params;
+    if(id >= projects.length) {
+        next();
+    }
     const projectName = projects[id].project_name;
-
 
     const { technologies, image_urls, description, github_link } = projects[id];
     const projInfo = { projectName, technologies, image_urls, description, github_link };
-    console.log(technologies.length);
-    console.log(projInfo);
-    if(id >= projects.length) {
-        req.params.id = 0
-        console.log(req.params.id);
-    } else {
-        return res.render('project', { projInfo });
-    }
+    return res.render('project', { projInfo });
 });
 
 module.exports = router;
